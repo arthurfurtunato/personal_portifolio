9cf254b50af7ebfdb23e2297288d23028499c4b4
arthurfurtunato <arthurfurtunato@gmail.com>
2021-10-15 16:29:13 -0700

    Criando a page do blog

--- personal_portifolio/urls.py
+++ personal_portifolio/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('', include('portifolio.urls'))
 ]
 
--- portifolio/views.py
+++ portifolio/views.py
@@ -3,6 +3,6 @@ from .models import Project
 
 def index(request):
     projects = Project.objects.all()
-    return render(request, 'index.html', {
+    return render(request, 'portfolio/index.html', {
         'projects': projects
     })
