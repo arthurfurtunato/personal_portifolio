6adeac216524b24681885fb01074fa5c070a679a
arthurfurtunato <arthurfurtunato@gmail.com>
2021-10-15 15:08:55 -0700

    admin, e aprendendo a interagir com a midia

--- portifolio/admin.py
+++ portifolio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+    list_display_links = ['title']
+
+admin.site.register(Project, ProjectAdmin)
