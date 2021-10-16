a111186477fe085901a772179b6bb12685befdff
arthurfurtunato <arthurfurtunato@gmail.com>
2021-10-15 19:18:06 -0700

    Ordenando os blogs por data publicada

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,7 @@ from .models import Blog
 
 # Create your views here.
 def index(request):
-    blog = Blog.objects.all()
-    
+    blog = Blog.objects.order_by('-date_post')[:3]
     return render(request, 'blog/index.html', {
         'blog': blog,
     })
