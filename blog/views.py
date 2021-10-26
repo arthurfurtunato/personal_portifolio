dd5584643acf48079fcab70b79653eefafa0d450
arthurfurtunato <arthurfurtunato@gmail.com>
2021-10-26 00:11:58 -0300

    Aplicacao do template e view detail

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+import blog
 from .models import Blog
 
 # Create your views here.
@@ -6,4 +7,10 @@ def index(request):
     blog = Blog.objects.order_by('-date_post')[:3]
     return render(request, 'blog/index.html', {
         'blog': blog,
+    })
+
+def detail(request, blog_id):
+    blog = Blog.objects.filter(id=blog_id).first
+    return render(request, 'blog/detail.html', {
+        'blog': blog
     })
