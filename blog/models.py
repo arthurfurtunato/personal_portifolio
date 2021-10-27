5a3bb1d19750574f7c4459734c95fd700f4d6a2a
arthurfurtunato <arthurfurtunato@gmail.com>
2021-10-26 21:18:26 -0300

    projeto finalizado, ajustando apenas os templates

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     date_post = models.DateField(auto_now_add=True)
-    description = models.TextField(max_length=200)
+    description = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Blog
 
 # Create your views here.
 def index(request):
-    blog = Blog.objects.order_by('-date_post')
+    blog = Blog.objects.order_by('-date_post')[:5]
     return render(request, 'blog/index.html', {
         'blog': blog,
     })
